77289eeb1cb41df57e1a8a03bd2a0330992442cd
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-08-17 17:18:32 -0400

    put all questions into an array '

--- aboutme.js
+++ aboutme.js
@@ -5,73 +5,28 @@ alert ('Hello ' + nam + ', really nice to meet you! We will start with some yes 
 // console.log(nam);
 document.write('Hey ' + nam + '! Thank you for answering the questions, and if you had some incorrect, No Worries! You will get to know more about myself in this page. Enjoy!');
 
-//Ask user if I am choosing Javascript for Code 401
-let path = prompt(' Am I doing Javascript for Code 401?');
-if (typeof (path) === 'string') {
-  path = path.toLowerCase(); // reassigns user variable to a lowercase string (if it is already a string).
-}
-//If "yes" is incorrect, if "no", it is correct
-if (path === 'no') {
-//   console.log('Correct!');
-  alert ('Correct!');
-} else if (path === 'yes') {
-//   console.log('Incorrect');
-}
+let questions = ['Am I doing Javascript for Code 401?', 'Is Google my favorite Tech Company?',
+  'Is becoming a Dev for Google my dream job?', 'Have I ever visited Yosemite National Park?',
+  'Do I consider Cheeseburger my favorite food?', 'Guess a number', 'Guess a state that I have lived in'];
+let answers = ['no','yes', 'yes', 'no', 'no', 9, ['north carolina', 'massachusetts', 'new york', 'rhode island']];
 
-//Ask if user if Google is my favorite tech company
-let tech = prompt('Is Google my favorite Tech Company?');
-if (typeof (tech) === 'string') {
-  tech = tech.toLowerCase(); // reassigns user variable to a lowercase string (if it is already a string).
-}
-// If yes, "Correct"
-if (tech === 'no') {
-//   console.log('Incorrect');
-} else if (tech === 'yes') {
-  alert('Correct!');
-//   console.log('Correct');
-}
+for (let i = 0; i < questions.length[i]; i++){
+  let response = prompt(questions.indexOf[0,1,2,3,4]);
 
-//Ask user if becoming a Software developer for Google is my dream job
-let career = prompt('Is becoming a Dev for Google my dream job?');
-if (typeof (career) === 'string') {
-  career = career.toLowerCase(); // reassigns user variable to a lowercase string (if it is already a string).
-}
-//In case of yes, Correct
-switch (career) {
-case 'yes':
-  alert('Correct!');
-  //   console.log('Correct');
-  break;
-case 'no':
-  break;
-}
+  if (typeof(answers[i]) !== 'string'){
 
-//Ask if I ever been to Yosemite Nattional Park
-let outdoors = prompt('Have I ever visited Yosemite National Park?');
-if (typeof (outdoors) === 'string') {
-  outdoors = outdoors.toLowerCase(); // reassigns user variable to a lowercase string (if it is already a string).
-}
-//In case of no, Correct
-switch (outdoors) {
-case 'yes':
-  break;
-case 'no':
-  alert('Correct!');
-  //   console.log('Correct');
-  break;
-}
-
-//Ask if Cheeseburger is my favorite food
-let food = prompt('Do I consider Cheeseburger my favorite food?');
-if (typeof (food) === 'string') {
-  food = food.toLowerCase(); // reassigns user variable to a lowercase string (if it is already a string).
-}
-//in the case of no, Correct
-switch (food) {
-case 'yes':
-  break;
-case 'no':
-  alert('Correct!');
-  //   console.log('Correct');
-  break;
+    for (let answer of answers[i]){
+      if(response.toLowerCase() === answer){
+        alert('Correct!');
+        break;
+      }
+    }
+  }
+  else {
+    if (response.toLowerCase() === answers[i]) {
+      alert('Correct!');
+    } else{
+      alert('Wrong!');
+    }
+  }
 }
