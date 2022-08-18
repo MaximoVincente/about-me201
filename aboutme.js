663c3fa4dd2420af6266881a28c97dd63fa454f6
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-08-17 21:31:47 -0400

    added score count

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,7 @@
 'use strict'; //strict syntax
 
+let score = 0;
+
 let nam = prompt('What is your name?');
 alert ('Hello ' + nam + ', really nice to meet you! We will start with some yes or no questions about myself :)');
 document.write('Hey ' + nam + '! Thank you for answering the questions, and if you had some incorrect, No Worries! You will get to know more about myself in this page. Enjoy!');
@@ -14,6 +16,7 @@ for (let i = 0; i < questions.length; i++){
   console.log(answers[i]);
   if (response.toLowerCase() === answers[i]) {
     alert('Correct!');
+    score ++;
   }else {
     alert('Wrong!');
   }
@@ -25,6 +28,7 @@ for(let i = 0; i < 4; i++){
   console.log('number of attempts');
   if (guess === number){
     alert('Correct!');
+    score ++
     break;
   }else if (guess > number){
     alert('TOO HIGH!');
@@ -44,6 +48,7 @@ for (let i = 0; i < 6; i++){
   let correct = false;
   for (let j = 0; j < states.length; j++){
     if (resp.toLowerCase() === states[j]){
+      score ++;
       alert('Correct! These are not easy states to guess!');
       correct = true;
       break;
@@ -56,3 +61,5 @@ for (let i = 0; i < 6; i++){
   }
 }
 alert('These were all the possible answers: New york, Massachussetts, Rhode Island, North Carolina, New hampshire');
+
+alert('You scored ' + score + ' out of 7');
