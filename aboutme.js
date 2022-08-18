ac11d4e4d5538e0963e894ccb279723fbb1292ae
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-08-17 21:17:14 -0400

    added multiple choice

--- aboutme.js
+++ aboutme.js
@@ -2,31 +2,57 @@
 
 let nam = prompt('What is your name?');
 alert ('Hello ' + nam + ', really nice to meet you! We will start with some yes or no questions about myself :)');
-// console.log(nam);
 document.write('Hey ' + nam + '! Thank you for answering the questions, and if you had some incorrect, No Worries! You will get to know more about myself in this page. Enjoy!');
 
 let questions = ['Am I doing Javascript for Code 401?', 'Is Google my favorite Tech Company?',
   'Is becoming a Dev for Google my dream job?', 'Have I ever visited Yosemite National Park?',
-  'Do I consider Cheeseburger my favorite food?', 'Guess a number', 'Guess a state that I have lived in'];
-let answers = ['no','yes', 'yes', 'no', 'no', 9, ['north carolina', 'massachusetts', 'new york', 'rhode island']];
+  'Do I consider Cheeseburger my favorite food?'];
+let answers = ['no','yes', 'yes', 'no', 'no'];
 
-for (let i = 0; i < questions.length[i]; i++){
-  let response = prompt(questions.indexOf[0,1,2,3,4]);
-
-  if (typeof(answers[i]) !== 'string'){
-
-    for (let answer of answers[i]){
-      if(response.toLowerCase() === answer){
-        alert('Correct!');
-        break;
-      }
-    }
-  }
-  else {
-    if (response.toLowerCase() === answers[i]) {
-      alert('Correct!');
-    } else{
-      alert('Wrong!');
-    }
+for (let i = 0; i < questions.length; i++){
+  let response = prompt(questions[i]);
+  console.log(answers[i]);
+  if (response.toLowerCase() === answers[i]) {
+    alert('Correct!');
+  }else {
+    alert('Wrong!');
   }
 }
+
+let number = 9;
+for(let i = 0; i < 4; i++){
+  let guess = parseInt(prompt('Guess a number'));
+  console.log('number of attempts');
+  if (guess === number){
+    alert('Correct!');
+    break;
+  }else if (guess > number){
+    alert('TOO HIGH!');
+  }else if(guess < number){
+    alert('too low');
+  }else{
+    alert('Not a number');
+  }
+}
+alert('The number was 9');
+
+
+let states = ['new york', 'massachusetts', 'rhode island', 'north carolina', 'new hampshire'];
+for (let i = 0; i < 6; i++){
+  let resp = prompt('Guess a state that I have lived in');
+  console.log('attempts of states');
+  let correct = false;
+  for (let j = 0; j < states.length; j++){
+    if (resp.toLowerCase() === states[j]){
+      alert('Correct! These are not easy states to guess!');
+      correct = true;
+      break;
+    }
+  }
+  if (correct === true){
+    break;
+  }else {
+    alert('Wrong!');
+  }
+}
+alert('These were all the possible answers: New york, Massachussetts, Rhode Island, North Carolina, New hampshire');
